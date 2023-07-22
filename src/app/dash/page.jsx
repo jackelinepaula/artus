a1277b4fad09f41e316f4fbcc95cf7a48932b163
douglasdans <douglas.dans@gmail.com>
2023-07-22 00:21:28 -0300

    Estrutura de pasta e mais alguns testes feitos

---
+++ src/app/dash/page.jsx
@@ -0,0 +1,8 @@
+import Navbar from "@/components/Dash/Navbar/Navbar";
+
+
+export default function Dash(){
+    return (
+        <Navbar/>
+    )
+}
---
+++ src/components/Dash/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css"
+
+export default function Navbar(){
+    return(
+        <div className="navbar-container">
+            <div className="logo-image">
+                <img src="@/../public/artus.svg"/>
+            </div>
+        </div>
+    )
+}
+
