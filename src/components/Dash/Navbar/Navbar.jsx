b4c3789fe63124e98c0dea744e79a5faaa9cb804
douglasdans <douglas.dans@gmail.com>
2023-07-26 21:36:22 -0300

    Tailwind Removido, component de navbar criado

--- src/components/Dash/Navbar/Navbar.jsx
+++ src/components/Dash/Navbar/Navbar.jsx
@@ -4,7 +4,12 @@ export default function Navbar(){
     return(
         <div className="navbar-container">
             <div className="logo-image">
+                artus
                 <img src="@/../public/artus.svg"/>
+            </div>
+            <div className="workspace-container">
+                <span>Workspace</span>
+                <button>+</button>
             </div>
         </div>
     )
